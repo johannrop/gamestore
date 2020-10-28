fb135a25db9da9da19cc3bd2338db8b6afcd541c
future <future@DESKTOP-N46TI9B>
2020-10-27 23:56:43 -0500

    conexion con la base de datos

---
+++ src/java/Configuracion/Conexion.java
@@ -0,0 +1,19 @@
+
+package Configuracion;
+
+import java.sql.*;
+
+public class Conexion {
+    Connection con;
+    public Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_gamestore","root","");            
+        } catch (Exception e) {
+            System.err.println("Error"+e);
+        }
+    }
+    public Connection getConnection(){
+        return con;
+    }
+}
